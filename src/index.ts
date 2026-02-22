b2697f28606c420c7e07c5d576e08f447bbb2ea0
Abraham <abrahamnhier97@gmail.com>
2026-02-22 15:33:57 +0300

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+const PORT = 8000;
+
+//cors options
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Backend server is running!");
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
